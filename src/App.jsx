0cd1f28f3d974404fa40f19a084775dc9b3f526b
Gabriela de Andrade <166560478+gabrieladeandradecode@users.noreply.github.com>
2026-08-07 17:14:31 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src="/logo.png" alt="Logo da Techboard" />
+      </header>
+      
+      <section>
+        <img src="/banner.png" alt="Banner de Tecnologia" />
+
+      </section>
+    </>
+  )
+}
+
+export default App

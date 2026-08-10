56967e270caea3061d9acacc727f2961c604856c
Gabriela de Andrade <166560478+gabrieladeandradecode@users.noreply.github.com>
2026-08-10 11:40:45 -0300

    component Form + style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 
+function Form() {
+  return (
+    <form action="" method="post" className='form'>
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">Qual o nome do evento?</label>
+        <input type="text" name="nome" placeholder='Summer dev hits' />
+        
+        <label htmlFor="">Data do evento</label>
+        <input type="text" placeholder='XX/XX/XXXX' />
+        
+        <label htmlFor="tema">Tema do evento</label>
+        <select name="temas" id="tema">
+          <option value="" disabled>Selecione uma opção..</option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+
+      </fieldset>
+    </form>
+  );
+}
+
 function App() {
   return (
     <>
-      <header>
-        <img src="/logo.png" alt="Logo da Techboard" />
-      </header>
-      
-      <section>
-        <img src="/banner.png" alt="Banner de Tecnologia" />
+    <main>
+        <header>
+          <img src="/logo.png" alt="Logo da Techboard" />
+        </header>
+        
+        <section className='section-tech'>
+          <img src="/banner.png" alt="Banner de Tecnologia" />
+        </section>
 
-      </section>
+        <Form/>
+    </main>
     </>
   )
 }
